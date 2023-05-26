3298e519868e08f7f7d47d5a481d4b799a7849f4
macarena <singhmacarena.r@gmail.com>
2023-05-26 09:18:11 -0300

    Added pre determined heigh and width for lazy load component

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,5 +1,6 @@
 import smilepurple from "../assets/smilepurple.svg";
 import { RegisterBtn } from "./RegisterBtn";
+import LazyLoad from 'react-lazy-load';
 
 export const CTA = () => {
   return (
@@ -8,22 +9,30 @@ export const CTA = () => {
         <h2 className="text-white text-[2rem] font-light"><span className="purple-text-highlight">Free</span> membership</h2>
         <div className="grid gap-y-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full gap-7 justify-items-center">
           <div className="cta-card">
-            <img className="pb-2" src={smilepurple} alt="" />
+            <LazyLoad height={36} width={45}>
+              <img className="pb-2" src={smilepurple} alt="Purple Smile Face" />
+            </LazyLoad> 
             <h4 className="font-semibold text-lg">Lorem ipsum</h4>
             <p className="text-sm font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="cta-card">
-            <img className="pb-2" src={smilepurple} alt="" />
+            <LazyLoad height={36} width={45}>
+              <img className="pb-2" src={smilepurple} alt="Purple Smile Face" />
+            </LazyLoad>
             <h4 className="font-semibold text-lg">Lorem ipsum</h4>
             <p className="text-sm font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="cta-card">
-            <img className="pb-2" src={smilepurple} alt="" />
+            <LazyLoad height={36} width={45}>
+              <img className="pb-2" src={smilepurple} alt="Purple Smile Face" />
+            </LazyLoad>
             <h4 className="font-semibold text-lg">Lorem ipsum</h4>
             <p className="text-sm font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
           <div className="cta-card">
-            <img className="pb-2" src={smilepurple} alt="" />
+            <LazyLoad height={36} width={45}>
+              <img className="pb-2" src={smilepurple} alt="Purple Smile Face" />
+            </LazyLoad>
             <h4 className="font-semibold text-lg">Lorem ipsum</h4>
             <p className="text-sm font-normal">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </div>
