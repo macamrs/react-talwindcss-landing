487a6450987365df47b1099db8d6662f0a5ee8d2
macarena <singhmacarena.r@gmail.com>
2023-05-26 17:25:55 -0300

    Added darker shadow to bottom hero component

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,7 +13,7 @@ export const Hero = () => {
   }, [])
 
   return (
-    <section className="hero-bg-image">
+    <section className="hero-bg-image relative">
       <div className='container mx-auto flex justify-evenly gap-[4.5rem] md:gap-[3rem] lg:gap-0 py-32 md:pb-12 lg:pb-0 md:pt-20  flex-col px-2 h-full'>
         <div className='flex justify-center items-center flex-col'>
           <h1 className='text-white pb-6 md:pb-1 font-bold text-[3rem] md:text-[6rem] lg:text-[7.5rem]'>Get schooled</h1>
@@ -26,7 +26,7 @@ export const Hero = () => {
           <RegisterBtn />
         </div>
 
-        <div className='flex flex-col gap-[3rem] md:gap-[4.5rem] min-h-[24rem]'>
+        <div className='flex flex-col gap-[3rem] md:gap-[4.5rem] min-h-[24rem] z-10'>
           <h3 className='text-white font-light text-[2rem] text-center'><span className='purple-text-highlight'>Learn</span> from the pros</h3>
           <motion.div 
           className='grid grid-cols-2 lg:grid-cols-4 w-full gap-7 justify-items-center'
@@ -54,6 +54,7 @@ export const Hero = () => {
           </motion.div >
         </div>
       </div>
+      <div className='bg-gradient-to-b from-transparent to-[#071629] h-2/4 w-full absolute bottom-0'></div>
     </section>
   )
 };
